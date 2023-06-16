0e001e1d05dbeb2de625b3be52e702c0cde2ca6a
Agatka <agata.mikla@gmail.com>
2023-06-16 20:12:28 +0200

    Add logger parser

--- app/draft_demo_logger.py
+++ app/draft_demo_logger.py
@@ -1,5 +1,53 @@
+from argparse import ArgumentParser
+from os import getenv
+from pathlib import Path
+
 from logger import Logger
 
+
+def parser():
+    parser = ArgumentParser(allow_abbrev=False)
+
+    parser.add_argument(
+        "--console-log-level",
+        dest="console_log_level",
+        default=getenv("CONSOLE_LOG_LEVEL", "DEBUG"),
+        help="Console logging level.",
+        type=str,
+    )
+    parser.add_argument(
+        "--file-log-level",
+        dest="file_log_level",
+        default=getenv("FILE_LOG_LEVEL", "INFO"),
+        help="File logging level.",
+        type=str,
+    )
+    parser.add_argument(
+        "--file-log-dir",
+        dest="file_log_dir",
+        default=getenv("FILE_LOG_DIR", str(Path().absolute() / "logs")),
+        help="A directory for logs storage.",
+        type=str,
+    )
+
+    args = parser.parse_args()
+    return args
+
 if __name__ == "__main__":
-    logger = Logger("name", "DEBUG", "DEBUG", "logs").logger
+    args = parser()
+
+    logger = Logger(
+        name=__name__,
+        console_log_level=args.console_log_level,
+        file_log_level=args.file_log_level,
+        file_log_dir=args.file_log_dir,
+    ).logger
+
     logger.debug("this is a debug message")
+    logger.info("this is INFO")
+
+
+
+
+
+
