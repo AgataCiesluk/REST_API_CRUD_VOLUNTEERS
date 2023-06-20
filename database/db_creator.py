89b69bfda85e4b30d218d6439ac7fe47edbfb6aa
Agatka <agata.mikla@gmail.com>
2023-06-20 21:46:24 +0200

    Cleanup refactor db_creator and db_handler

--- database/db_creator.py
+++ database/db_creator.py
@@ -2,13 +2,67 @@
  as well as methods needed to create volunteers table with exemplary data.
 
  This module doesn't have to be used if db_volunteer already exist and contains volunteer table with exemplary rows."""
-
-from dotenv import dotenv_values
-
-config = dotenv_values("../.env")
-
-
-#### Create DB db_volunteers if not exist ####
+# import logging
+#
+# import psycopg2
+# from dotenv import dotenv_values
+# from psycopg2 import OperationalError
+#
+# from database.db_config import DB_CONNECTION_ARGS
+#
+# config = dotenv_values("../.env")
+#
+#
+# def create_connection(db_name, db_user, db_password, db_host, db_port):
+#     connection = None
+#     try:
+#         connection = psycopg2.connect(
+#             database=db_name,
+#             user=db_user,
+#             password=db_password,
+#             host=db_host,
+#             port=db_port,
+#         )
+#         print("Connection to PostgreSQL DB successful")
+#     except OperationalError as e:
+#         print(f"The error '{e}' occurred")
+#     return connection
+#
+#
+# def execute_query(connection, query, insert_data=None):
+#     connection.autocommit = True
+#     cursor = connection.cursor()
+#     try:
+#         if insert_data:
+#             cursor.execute(query, insert_data)
+#         else:
+#             cursor.execute(query)
+#             query_result = cursor.fetchall()
+#             if query_result:
+#                 return query_result
+#         print("Query executed successfully")
+#     except OperationalError as e:
+#         print(f"The error '{e}' occurred")
+#     finally:
+#         cursor.close()
+#         connection.close()
+#
+#
+# def insert_data_into_volunteers_table(data: list):
+#     if not data:
+#         raise ValueError("Data list should not be empty.")
+#     connection = create_connection(*DB_CONNECTION_ARGS)
+#     data_records = ", ".join(["%s"] * len(data))
+#     insert_query = (
+#         f"INSERT INTO volunteers (first_name, last_name, phone_number) VALUES ({data_records})"
+#     )
+#     execute_query(connection, insert_query, data)
+#     success_info = f"Data inserted into volunteers table in DB db_volunteers."
+#     logging.info(success_info)
+#     print(success_info)
+#
+#
+# ### Create DB db_volunteers if not exist ####
 # def create_db(db_name):
 #     conn_postgres = create_connection(
 #         "postgres",
@@ -16,7 +70,7 @@ config = dotenv_values("../.env")
 #         config.get("DB_PASSWORD"),
 #         config.get("DB_HOST"),
 #         config.get("DB_PORT"))
-#     execute_query_without_return(conn_postgres, f"CREATE DATABASE {db_name}")
+#     execute_query(conn_postgres, f"CREATE DATABASE {db_name}")
 #     conn_postgres.close()
 #     success_info = f"DB {db_name} created."
 #     logging.info(success_info)
@@ -24,15 +78,12 @@ config = dotenv_values("../.env")
 #
 #
 # create_db(config.get('DB_NAME'))
-
-
-
-### Connect to DB db_volunteers if this DB exists ###
+#
+# ## Connect to DB db_volunteers if this DB exists ###
 # conn_volunteers = create_connection(*DB_CONNECTION_ARGS)
 # conn_volunteers.close()
-
-
-### Create volunteers table in DB db_volunteers if this table doesn't exist ###
+#
+# ## Create volunteers table in DB db_volunteers if this table doesn't exist ###
 # conn_volunteers = create_connection(*DB_CONNECTION_ARGS)
 # create_volunteers_table_query = """
 # CREATE TABLE IF NOT EXISTS volunteers (
@@ -44,9 +95,8 @@ config = dotenv_values("../.env")
 # """
 #
 # execute_query(conn_volunteers, create_volunteers_table_query)
-
-
-### Add exemplary volunteers to voluneetrs table in DB db_volunteers ###
+#
+# ## Add exemplary volunteers to voluneetrs table in DB db_volunteers ###
 # volunteers = [
 #     ("Agata", "Christie", "555-999-222"),
 #     ("James", "Smith", "555-666-222"),
--- database/db_handler.py
+++ database/db_handler.py
@@ -1,65 +1,8 @@
-import logging
-
-import psycopg2
-from psycopg2 import OperationalError
-from dotenv import dotenv_values
 from sqlalchemy import exc
 
-from database.db_config import DB_CONNECTION_ARGS
-from models import Volunteer
 from exceptions import NoRecordFound
+from models import Volunteer
 from utils import models_list_to_dict
-
-config = dotenv_values("../.env")
-
-
-def create_connection(db_name, db_user, db_password, db_host, db_port):
-    connection = None
-    try:
-        connection = psycopg2.connect(
-            database=db_name,
-            user=db_user,
-            password=db_password,
-            host=db_host,
-            port=db_port,
-        )
-        print("Connection to PostgreSQL DB successful")
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-    return connection
-
-
-def execute_query(connection, query, insert_data=None):
-    connection.autocommit = True
-    cursor = connection.cursor()
-    try:
-        if insert_data:
-            cursor.execute(query, insert_data)
-        else:
-            cursor.execute(query)
-            query_result = cursor.fetchall()
-            if query_result:
-                return query_result
-        print("Query executed successfully")
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-    finally:
-        cursor.close()
-        connection.close()
-
-
-def insert_data_into_volunteers_table(data: list):
-    if not data:
-        raise ValueError("Data list should not be empty.")
-    connection = create_connection(*DB_CONNECTION_ARGS)
-    data_records = ", ".join(["%s"] * len(data))
-    insert_query = (
-        f"INSERT INTO volunteers (first_name, last_name, phone_number) VALUES ({data_records})"
-    )
-    execute_query(connection, insert_query, data)
-    success_info = f"Data inserted into volunteers table in DB db_volunteers."
-    logging.info(success_info)
-    print(success_info)
 
 
 def db_add_volunteer(first_name, last_name, phone_number, db_model):
@@ -78,6 +21,7 @@ def db_delete_volunteer(id, db_model):
     db_model.session.commit()
     return volunteer
 
+
 def db_update_volunteer(id, db_model, first_name=None, last_name=None, phone_number=None):
     try:
         volunteer = get_volunteer_by_id(id)
@@ -93,37 +37,16 @@ def db_update_volunteer(id, db_model, first_name=None, last_name=None, phone_num
     db_model.session.commit()
     return volunteer
 
+
 def get_all_volunteers():
     all_volunteers = Volunteer.query.all()
     return models_list_to_dict(all_volunteers)
-
-
-def get_volunteer_by(param_name, param_value, connection):
-    query = f"SELECT * FROM volunteers WHERE {param_name}={param_value}"
-    volunteers_by_param = execute_query(connection, query)
-    return volunteers_by_param
 
 
 def get_volunteer_by_id(volunteer_id: int):
     try:
         volunteer = Volunteer.query.filter_by(volunteer_id=volunteer_id).one()
     except exc.NoResultFound as e:
-        #     TO_DO: Logger with original error message logger.warning(e.message)
+        #     TODO: Logger with original error message logger.warning(e.message)
         raise NoRecordFound
     return volunteer
-
-# new_volunteer_attributes = {'first_name': 'Mateuszek'}
-# volunteer = get_volunteer_by_id(12)
-# print(volunteer.first_name)
-# print(volunteer.last_name)
-# volunteer_dict = volunteer.__dict__
-# for attr_name, attr_value in new_volunteer_attributes.items():
-#     if attr_value:
-#         volunteer_dict[attr_name] = attr_value
-# volunteer = volunteer_dict
-# print(volunteer.first_name)
-# print(volunteer.last_name)
-# Funkcja niech wyrzuca bledy np. raise valueError kiedy tabela jest pusta lub nie istnieje lub gdy nie udalo sie polaczenie z baza danych.
-# Chociaz polaczenie z baza danych juz jest sprawdzane w create connection try/except.
-# Controller powinien przechwycic te bledy i przekazac klientowi numer bledu np. 405 wraz z trescia np. pusta baza danych etc.
-# Dzieki temu jesli w db_handler cos sie skopie to po prostu wypycha blad do controllera a controller ten blad odpowiednio obrobi i wyrzuci do klienta.
